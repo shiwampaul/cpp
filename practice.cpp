ef0b5e8e433255d16f86579b887f7a70c4d63f89
shiwampaul <shiwampaul@gmail.com>
2026-09-11 10:12:53 +0530

    linked list creation using for loop

--- practice.cpp
+++ practice.cpp
@@ -6,38 +6,32 @@ struct Node{
     Node* next;
 };
 int main() {
+    Node* head = nullptr;
+    Node* temp = nullptr;
+    for (int i=0; i<10; i++){
+        Node* newdata = new Node;
+
+        cin >> newdata->data;
+        newdata->next= nullptr;
+        if ( head == nullptr){
+            head = newdata;
+            temp = newdata;
+
+        }
+        else{
+            temp->next=newdata;
+            temp = newdata;
+        }
+    }
+    temp = head;
     
-    Node* n1= new Node;
-    Node* n2 = new Node;
-    Node* n3 = new Node;
-    Node* n4 = new Node;
-    Node* n5 = new Node;
-
-    n1->data = 10;
-    n2->data = 209;
-    n3->data = 399;
-    n4->data = 34;
-    n5->data = 54;
-
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    n5->next = NULL;
     
-    Node* head = n1;
-    //Node* temp = head;
-    //delete n1;
-    //cout << n1->data<<endl;
-
-    while (head->next != nullptr){
-        cout << head->data<< " ";
-        head = head->next;
+    while (temp != nullptr){
+        cout << temp->data<< " ";
+        temp = temp->next;
     }
 
-    //delete head;
-    cout<<endl;
-    cout << n5->data;
+   
 
     return 0;
 }
