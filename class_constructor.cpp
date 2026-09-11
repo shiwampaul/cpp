d491170e89897e65accd2400694be6d64ce9559a
shiwampaul <shiwampaul@gmail.com>
2026-09-11 22:42:36 +0530

    This is the final code is the fastest

--- class_constructor.cpp
+++ class_constructor.cpp
@@ -8,20 +8,18 @@ class Shivam{
     string message;
     double fees;
     public:
-    Shivam(string name, string standard, int marks, string message, double fees){
+    Shivam(){
         cout<< "Enter name: " ; cin >> this->name;
         cout << "Enter class: "; cin >> this->standard;
         cout << "Enter marks: "; cin >> this->marks;
-        cout << "Enter message: "; getline(cin,this->message);
         cout << "Enter fees: "; cin >> this->fees;
     }
     void submit_fees(double amount);
-    void Shivam::update_marks(int mark, int option);
+    void update_marks(int mark, int option);
     ~Shivam(){
         cout << "Name of the student: "<< name<< endl;
         cout << "Current class of student: "<< standard<< endl;
-        cout << "Marks secured by student: "<< marks;
-        cout << "Message for student: "<< message<< endl;
+        cout << "Marks secured by student: "<< marks<<endl;
         cout << "Current fees of student: "<<fees<< endl;
     }
 
@@ -32,7 +30,7 @@ void Shivam::submit_fees(double amount){
 void Shivam::update_marks(int mark, int option){
     cout << "Choose :"<<endl;
     cout << "1. To increase Marks"<<endl;
-    cout << "2. To decrease Marks";
+    cout << "2. To decrease Marks"<<endl;
     if (option == 1){
         marks += mark;
     }
@@ -42,6 +40,19 @@ void Shivam::update_marks(int mark, int option){
 }
 
 int main() {
+    Shivam s;
+
+    int marks, option;
+    cout << "Enter marks and option 1 or 0 to increase and decrease marks"<< endl;
+    cin >> marks >>  option;
+    double amount;
+    cout << "Enter amount you want to deposit"<< endl;
+    cin >> amount;
+
+    s.submit_fees(amount);
+    s.update_marks(marks, option);
+
+    
       
     return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -6,30 +6,30 @@ struct Node{
     Node* next;
 };
 int main() {
-    Node* head = nullptr;
-    Node* temp = nullptr;
-    for (int i=0; i<10; i++){
-        Node* newdata = new Node;
+Node* head = nullptr;
+Node* temp = nullptr;
+for (int i=0; i<10; i++){
+    Node* newdata = new Node;
 
-        cin >> newdata->data;
-        newdata->next= nullptr;
-        if ( head == nullptr){
-            head = newdata;
-            temp = newdata;
+    cin >> newdata->data;
+    newdata->next= nullptr;
+    if ( head == nullptr){
+        head = newdata;
+        temp = newdata;
 
-        }
-        else{
-            temp->next=newdata;
-            temp = newdata;
-        }
     }
-    temp = head;
-    
-    
-    while (temp != nullptr){
-        cout << temp->data<< " ";
-        temp = temp->next;
+    else{
+        temp->next=newdata;
+        temp = newdata;
     }
+}
+temp = head;
+
+
+while (temp != nullptr){
+    cout << temp->data<< " ";
+    temp = temp->next;
+}
 
    
 
