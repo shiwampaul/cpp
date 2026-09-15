05976fb064f37383dcb997b2a8af12ff31909255
shiwampaul <shiwampaul@gmail.com>
2026-09-15 12:47:59 +0530

    created first linked list

--- linked_list_creation.cpp
+++ linked_list_creation.cpp
@@ -5,21 +5,32 @@ struct Node{
     int data;
     Node* next;
 };
+void insertEnd(Node* &head, int value){
+    Node* newNode = new Node{value, nullptr};
+
+    if (head == nullptr){
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
 
 int main() {
     Node* head = nullptr;
-    Node* temp = nullptr;
-    for (int i=0; i<10; i++){
-        Node* element = new Node{i, element};
-
-        
-
-        head = element;
-        temp = element;
-
-        while(true){
-
+    for (int i=1; i<=5; i++){
+        insertEnd(head, i*10);
     }
+
+    while (head != nullptr){
+        cout<< head->data<< " ";
+        head = head->next;
+    }
+
+    
     
       
     return 0;
